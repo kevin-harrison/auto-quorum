e31e010b48ef4cf5a1c868bdf58751f373bfb6de
kevin <kevin@harrisoncentral.com>
2024-03-30 14:04:52 +0100

    Adds optimize flag and artificial latency

--- build_scripts/run-local-client.sh
+++ build_scripts/run-local-client.sh
@@ -1,4 +1,4 @@
-usage="Usage: run-local-client.sh server_id server_id"
+usage="Usage: run-local-client.sh server_id server_id optimize_setting"
 [ -z "$1" ] &&  echo "No server_id given! $usage" && exit 1
 [ -z "$2" ] &&  echo "No server_id given! $usage" && exit 1
 
@@ -6,25 +6,31 @@ server_id=$1
 other_id=$2
 client1_config_path="./client-${server_id}-config.toml"
 client2_config_path="./client-${other_id}-config.toml"
+if [ -z "$3" ]; then
+    client1_log_path="../../auto-quorum-benchmark/logs/test-local_client-${server_id}.log"
+    client2_log_path="../../auto-quorum-benchmark/logs/test-local_client-${other_id}.log"
+else
+    client1_log_path="../../auto-quorum-benchmark/logs/test-local-no-reconfig_client-${server_id}.log"
+    client2_log_path="../../auto-quorum-benchmark/logs/test-local-no-reconfig_client-${other_id}.log"
+fi
 
 sync_buffer=1000
 utc_sync=$(($(date +%s%3N) + sync_buffer))
 
 interrupt() {
     pkill -P $$
-    cleanup_logs
+    cleanup
 }
-cleanup_logs() {
+cleanup() {
     sed -i "s/$utc_sync/SCHEDULE/g" "$client1_config_path"
     sed -i "s/$utc_sync/SCHEDULE/g" "$client2_config_path"
 }
 trap "interrupt" SIGINT
-trap "cleanup_logs" EXIT
-
+trap "cleanup" EXIT
 
 sed -i "s/SCHEDULE/$utc_sync/g" "$client1_config_path" &&
 sed -i "s/SCHEDULE/$utc_sync/g" "$client2_config_path" &&
 
-CONFIG_FILE="$client1_config_path"  cargo run --manifest-path="../omnipaxos_client/Cargo.toml" 1> ../../auto-quorum-benchmark/logs/test-local_client-${server_id}.log &
-CONFIG_FILE="$client2_config_path"  cargo run --manifest-path="../omnipaxos_client/Cargo.toml" 1> ../../auto-quorum-benchmark/logs/test-local_client-${other_id}.log
+CONFIG_FILE="$client1_config_path"  cargo run --manifest-path="../omnipaxos_client/Cargo.toml" 1> "$client1_log_path" &
+CONFIG_FILE="$client2_config_path"  cargo run --manifest-path="../omnipaxos_client/Cargo.toml" 1> "$client2_log_path"
 
--- build_scripts/run-local-cluster.sh
+++ build_scripts/run-local-cluster.sh
@@ -1,17 +1,33 @@
-usage="Usage: run-local-cluster.sh cluster_size"
+usage="Usage: run-local-cluster.sh cluster_size optimize_setting"
 [ -z "$1" ] &&  echo "No cluster_size given! $usage" && exit 1
-
-kill_children() {
-    pkill -P $$
-}
-trap "kill_children" SIGINT
-
 cluster_size=$1
 
-rm -rf ../../auto-quorum-benchmark/logs/test-local*
+interrupt() {
+    pkill -P $$
+    cleanup
+}
+cleanup() {
+    for ((i = 1; i <= cluster_size; i++)); do
+        config_path="./server-${i}-config.toml"
+        sed -i "s/optimize = true/optimize = OPTIMIZE/g" "$config_path"
+        sed -i "s/optimize = false/optimize = OPTIMIZE/g" "$config_path"
+    done
+    sudo tc qdisc del dev lo root
+}
+trap "interrupt" SIGINT
+trap "cleanup" EXIT
+
+sudo tc qdisc add dev lo root netem delay 100msec
 for ((i = 1; i <= cluster_size; i++)); do
-    RUST_LOG=debug CONFIG_FILE=server-${i}-config.toml cargo run --manifest-path="../omnipaxos_server/Cargo.toml" 1> ../../auto-quorum-benchmark/logs/test-local_server-${i}.log &
+    config_path="./server-${i}-config.toml"
+    if [ -z "$2" ]; then
+        sed -i "s/OPTIMIZE/true/g" "$config_path" &&
+        log_path="../../auto-quorum-benchmark/logs/test-local_server-${i}.log"
+    else
+        sed -i "s/OPTIMIZE/false/g" "$config_path" &&
+        log_path="../../auto-quorum-benchmark/logs/test-local-no-reconfig_server-${i}.log"
+    fi
+    RUST_LOG=debug CONFIG_FILE="$config_path" cargo run --manifest-path="../omnipaxos_server/Cargo.toml" 1> "$log_path" &
 done
 wait
-
 
